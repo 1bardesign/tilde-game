f5a1dcc54e2cb77440e8866317e00625efe2295a
Max Cahill <1bardesign@gmail.com>
2021-07-16 15:57:54 +1000

    made snake chase player when within a certain distance

--- ohno_a_snake.lua
+++ ohno_a_snake.lua
@@ -96,34 +96,48 @@ function snake:tick()
 		self.choice_delay = 0;
 
 		-- direction choices
-		local directions = { self.dir, self.dir, self.dir, self.dir, self.dir, self.dir, self.dir, self.dir, vec2( 0, 1 ), vec2( 0, -1 ), vec2( 1, 0 ), vec2( -1, 0 ) };
-		while #directions > 0 do
-			local direction = tablex.take_random( directions );
-			local new_head_pos = self.parts[1].pos:vadd( direction );
-			if can_enter_cell( self.parts, self.game_state.grid, new_head_pos.x, new_head_pos.y ) then
-				sounds.serpent_move:play()
+		local directions = table.shuffle({
+			--bias current dir
+			self.dir, self.dir, self.dir, self.dir,
+			--cardinal
+			vec2(0, 1), vec2(0, -1), vec2(1, 0), vec2(-1, 0)
+		})
+		local player_direction, player_distance = self.game_state.player.pos:vsub(self.parts[1].pos):normalisei_both()
+		local player_near = player_distance < 20
+		local chosen_dir = functional.find_best(directions, function(d)
+			local new_head_pos = self.parts[1].pos:vadd(d)
+			if not can_enter_cell(self.parts, self.game_state.grid, new_head_pos.x, new_head_pos.y) then
+				return nil
+			end
+			--not near player, just wander
+			if not player_near then
+				return love.math.random()
+			end
+			--near player, take direction into account
+			return d:dot(player_direction) + love.math.random()
+		end)
+		if chosen_dir then
+			sounds.serpent_move:play()
 
-				self.dir = direction;
+			self.dir = chosen_dir
 
-				for i=#self.parts,1,-1 do
-					-- Move to new position
-					local p = self.parts[i];
-					local new_pos = i == 1 and new_head_pos or self.parts[i - 1].pos;
-					p.prev_pos = p.pos;
-					p.pos = new_pos;
-				end
-				
-				for i=1,#self.parts do
-					local p = self.parts[i];
-					p.dir = i == 1 and self.dir or self.parts[i-1].pos:vsub(p.pos);
-				end
+			for i = #self.parts, 1, -1 do
+				-- Move to new position
+				local p = self.parts[i];
+				local new_pos = i == 1 and p.pos:vadd(self.dir) or self.parts[i - 1].pos;
+				p.prev_pos = p.pos;
+				p.pos = new_pos;
+			end
 
-				break;
+			for i = 1, #self.parts do
+				local p = self.parts[i];
+				p.dir = i == 1 and self.dir or self.parts[i-1].pos:vsub(p.pos);
 			end
 		end
+		self.choice_delay = 3;
 	else
 		-- Growl 
-		self.choice_delay = 2;
+		self.choice_delay = 5;
 		sounds.serpent_growl:play()
 	end
 end
