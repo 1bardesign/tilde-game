6b0df5805febf07512b399262027705d24561dfd
Max Cahill <1bardesign@gmail.com>
2021-07-16 11:07:40 +1000

    added ascii3d renderer and ultra basic gameobject separation

---
+++ ascii3d.lua
@@ -0,0 +1,67 @@
+--[[
+	z sorted "3d" ascii renderer
+
+	chuck glyphs at it with xyz positions, they'll be drawn properly sorted as a big batch
+]]
+
+local texture = love.graphics.newImage("cga8x8thick.png")
+texture:setFilter("nearest", "nearest")
+
+local quad = love.graphics.newQuad(0,0,0,0,texture:getDimensions())
+
+local texture_geometry = vec2(16, 16)
+local tile_size = vec2(texture:getDimensions()):vdivi(texture_geometry)
+
+local ascii3d = class()
+
+function ascii3d:new()
+	self.queue = {}
+end
+
+function ascii3d:add(x, y, z, glyph, colour, rotation, scale)
+	table.insert(self.queue, {x, y, z, glyph, colour, rotation or 0, scale or 1})
+end
+
+local function z_sort(a, b)
+	--z
+	if a[3] < b[3] then return true end
+	--y
+	if a[2] < b[2] then return true end
+	return false
+end
+
+function ascii3d:draw()
+	table.stable_sort(self.queue, z_sort)
+	love.graphics.push("all")
+	for _, v in ipairs(self.queue) do
+		local x, y, z, glyph, colour, rotation, scale = table.unpack7(v)
+		if z > 1 then
+			-- TODO: Make blurry / faded / something
+			--(likely write out z to a separate mask for various effects)
+		end
+		local sx = math.floor(x * tile_size.x)
+		local sy = math.floor((y - z) * tile_size.y)
+		local b = glyph:byte(1)
+		local tx = math.floor(b % texture_geometry.x)
+		local ty = math.floor(b / texture_geometry.x)
+		quad:setViewport(
+			tx * tile_size.x,
+			ty * tile_size.y,
+			tile_size.x,
+			tile_size.y
+		)
+		love.graphics.setColor(color.unpack_argb(colour))
+		love.graphics.draw(
+			texture, quad,
+			sx, sy,
+			rotation,
+			scale, scale,
+			tile_size.x / 2,
+			tile_size.x / 2
+		)
+	end
+	love.graphics.pop()
+	self.queue = {}
+end
+
+return ascii3d
--- game_state.lua
+++ game_state.lua
@@ -12,7 +12,9 @@ end
 
 function state:enter()
 	--setup anything to be done on state enter here (ie reset everything)
+	self.display = require("ascii3d")()
 	self.grid = grid(20, 10)
+	self.objects = {}
 
 	for y = 1, self.grid.size.y do
 		for x = 1, self.grid.size.x do
@@ -33,15 +35,31 @@ function state:enter()
 	end
 
 	self.player_pos = vec2(10, 5)
-	self.grid:clear(self.player_pos:unpack())
-	self.grid:set_template(
-		self.player_pos.x, self.player_pos.y,
-		template.player
-	)
+	--temporary inline player gameobject
+	table.insert(self.objects, {
+		pos = self.player_pos,
+		template = template.player,
+		grid = self.grid, --todo: refactor, we only need this for template parsing
+		update = function(self)
+			if love.keyboard.isDown("up", "w") then self.pos:saddi(0, -1) end
+			if love.keyboard.isDown("down", "s") then self.pos:saddi(0, 1) end
+			if love.keyboard.isDown("left", "a") then self.pos:saddi(-1, 0) end
+			if love.keyboard.isDown("right", "d") then self.pos:saddi(1, 0) end
+		end,
+		draw = function(self, display)
+			local x, y = self.pos:vmul(self.grid.cell_size):unpack()
+			grid:parse_template(self.template, function(ox, oy, z, glyph, colour)
+				display:add(x + ox, y + oy, z, glyph, colour)
+			end)
+		end,
+	})
 end
 
 function state:update(dt)
 	--update each tick
+	for _, v in ipairs(self.objects) do
+		v:update()
+	end
 end
 
 function state:draw()
@@ -51,10 +69,14 @@ function state:draw()
 	)
 	love.graphics.scale(2)
 	love.graphics.translate(
-		self.player_pos:vmul(grid.cell_size):smuli(-1):roundi():unpack()
+		self.player_pos:vmul(grid.cell_size):smuli(8):smuli(-1):roundi():unpack()
 	)
 	love.graphics.clear(colour.unpack_argb(self.background_colour))
-	self.grid:draw()
+	self.grid:draw(self.display)
+	for _, v in ipairs(self.objects) do
+		v:draw(self.display)
+	end
+	self.display:draw()
 end
 
 function state:keypressed(k)
