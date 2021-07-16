2f568e373400e21e5da4bf8d853ceccd6f441efe
Max Cahill <1bardesign@gmail.com>
2021-07-16 12:04:02 +1000

    refactored grid to store a solid mask and template instead of individual tiles

--- generate_world.lua
+++ generate_world.lua
@@ -7,17 +7,21 @@ return function(game_state)
 	for y = 1, grid.size.y do
 		for x = 1, grid.size.x do
 			local t = template.grass
+			local s = false
 			local r = love.math.random();
 			if r < 0.1 then
 				t = template.flowers
 			elseif r < 0.2 then
 				t = template.tree
+				s = true
 			elseif r < 0.22 then
 				t = template.rocks
+				s = true
 			end
-			grid:set_template(
+			grid:set(
 				x, y,
-				table.pick_random(t)
+				table.pick_random(t),
+				s
 			)
 		end
 	end
--- grid.lua
+++ grid.lua
@@ -1,5 +1,5 @@
 --[[
-	grid of tiles of cells
+	grid of cells that store a template for rendering and a solid mask
 
 	written in a pretty inefficient way for now :)
 ]]
@@ -7,120 +7,8 @@
 --config
 local cell_size = vec2(3, 3)
 
---
-local tile = class({
-	name = "tile",
-})
-
-function tile:new(ox, oy, z, glyph, colour)
-	self.offset = vec2(ox, oy)
-	self.z = z
-	self.glyph = glyph
-	self.colour = colour
-end
-
-function tile:draw(x, y, display)
-	display:add(
-		x + self.offset.x,
-		y + self.offset.y,
-		self.z,
-		self.glyph,
-		self.colour
-	)
-end
-
-function tile:set(glyph, colour)
-	self.glyph = glyph
-	self.colour = colour
-end
-
-function tile.compare_z(a, b)
-	return a.z < b.z
-end
-
-local cell = class({
-	name = "cell",
-})
-
-function cell:new(x, y)
-	self.pos = vec2(x, y) --could be implicit
-	self.tiles = {}
-end
-
-function cell:draw(display)
-	local x = self.pos.x * cell_size.x
-	local y = self.pos.y * cell_size.y
-	for _, v in ipairs(self.tiles) do
-		v:draw(x, y, display)
-	end
-end
-
-function cell:tile(ox, oy, z)
-	return functional.find_match(self.tiles, function(v)
-		return v.offset.x == ox and v.offset.y == oy and v.z == z
-	end)
-end
-
-function cell:set(ox, oy, z, glyph, colour)
-	local to_remove =
-		glyph == nil
-		or glyph == ""
-		or glyph == " "
-
-	local t = self:tile(ox, oy, z)
-	if t then
-		if to_remove then
-			table.remove_value(self.tiles, t)
-		else
-			t:set(glyph, colour or 0xffffffff)
-		end
-		return
-	end
-	if not to_remove then
-		table.insert(self.tiles, tile(ox, oy, z, glyph, colour))
-	end
-end
-
-function cell:clear()
-	self.tiles = {}
-end
-
-local grid = class()
-
-function grid:new(w, h)
-	self.size = vec2(w, h)
-	self.cells = functional.generate(self.size.y, function(y)
-		return functional.generate(self.size.x, function(x)
-			return cell(x, y)
-		end)
-	end)
-end
-
-function grid:draw(display)
-	for _, row in ipairs(self.cells) do
-		for _, v in ipairs(row) do
-			v:draw(display)
-		end
-	end
-end
-
-function grid:cell(x, y)
-	x = math.floor(x)
-	y = math.floor(y)
-	assert(x > 0 and x <= self.size.x, "x out of range")
-	assert(y > 0 and y <= self.size.y, "y out of range")
-	return self.cells[y][x]
-end
-
-function grid:clear(x, y)
-	self:cell(x, y):clear()
-end
-
-function grid:set(x, y, ox, oy, z, glyph, colour)
-	self:cell(x, y):set(ox, oy, z, glyph, colour)
-end
-
-function grid:parse_template(template, f)
+--doesn't actually rely on grid
+local function parse_template(template, f)
 	local z = #template - 1
 	for _, lines in ipairs(template) do
 		local longest_line = functional.find_max(lines, function(v)
@@ -156,15 +44,78 @@ function grid:parse_template(template, f)
 	end
 end
 
-function grid:set_template(x, y, template)
-	local cell = self:cell(x, y)
-	grid:parse_template(template, function(ox, oy, z, glyph, colour)
-		cell:set(ox, oy, z, glyph, colour)
+--cells are now fairly vestigial, could be refactored into just grid
+local cell = class({
+	name = "cell",
+})
+
+function cell:new(x, y)
+	self.pos = vec2(x, y) --could be implicit
+	self.template = false
+	self.solid = false
+end
+
+function cell:draw(display)
+	if not self.template then
+		return
+	end
+	local x = self.pos.x * cell_size.x
+	local y = self.pos.y * cell_size.y
+	parse_template(self.template, function(ox, oy, z, glyph, colour)
+		display:add(x + ox, y + oy, z, glyph, colour)
 	end)
+end
+
+local grid = class({
+	name = "grid",
+})
+
+function grid:new(w, h)
+	self.size = vec2(w, h)
+	self.cells = functional.generate(self.size.y, function(y)
+		return functional.generate(self.size.x, function(x)
+			return cell(x, y)
+		end)
+	end)
+end
+
+function grid:draw(display)
+	for _, row in ipairs(self.cells) do
+		for _, v in ipairs(row) do
+			v:draw(display)
+		end
+	end
+end
+
+function grid:cell(x, y)
+	x = math.floor(x)
+	y = math.floor(y)
+	assert(self:in_bounds(x, y), "attempt to get cell out of bounds")
+	return self.cells[y][x]
+end
+
+function grid:clear(x, y)
+	self:cell(x, y):clear()
+end
+
+function grid:set(x, y, template, solid)
+	local cell = self:cell(x, y)
+	cell.template = template
+	cell.solid = solid
+end
+
+function grid:in_bounds(x, y)
+	return (x > 0 and x <= self.size.x)
+		and (y > 0 and y <= self.size.y)
+end
+
+function grid:solid_at(x, y)
+	return not self:in_bounds(x, y) or self:cell(x, y).solid
 end
 
 --export
 grid.cell_size = cell_size
+grid.parse_template = parse_template
 
 return grid
 
