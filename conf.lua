4fa0a88e4aa42ace20044eb25cae9c9e687d4b90
Ben <benjamin.porter@gmail.com>
2021-07-20 09:58:07 +1000

    Added icon.ico, minor start/end fix

--- conf.lua
+++ conf.lua
@@ -1,15 +1,14 @@
 function love.conf(t)
-	local name = "forest"
+	local name = "~"
 
 	t.version = "11.3"
 
 	t.window.title = name
 	t.identity = name
-	-- t.window.icon = "assets/icon.png"
+	t.window.icon = "raw/icon.png"
 
 	-- commandline output for windows
 	t.console = true
 
-	--720p window for now
 	t.window.fullscreen = "desktop"
 end
