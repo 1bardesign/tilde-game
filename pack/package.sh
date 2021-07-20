fe8d44924bea6e6e14f89772fffc730685c84652
Max Cahill <1bardesign@gmail.com>
2021-07-20 15:57:06 +1000

    build script changes

--- pack/package.sh
+++ pack/package.sh
@@ -10,14 +10,13 @@ cd ..
 zip -r pack/tilde.love *.lua src assets lib config
 cd pack
 
-
 #windows
 mkdir dist
 cat ./win/love.exe tilde.love > dist/tilde.exe
 cp ./win/*.dll dist
 cp ./win/license.txt dist/license_love2d.txt
 cp ../readme-public.md dist/readme.md
-cd dist
-zip -r ../tilde-windows.zip .
-cd ..
-rm -rf dist
+
+mv dist tilde
+zip -r tilde-windows.zip tilde
+rm -rf tilde
